de8ad95fa9af0ce43dd0d0bf44d17865133ad724
Fabian Irsara <info@fabianirsara.com>
2013-08-09 15:20:20 +0200

    export db-structure

--- application/config/db.sql
+++ application/config/db.sql
@@ -4,16 +4,20 @@ CREATE TABLE `hates` (
   `project_id` int(11) unsigned NOT NULL,
   `ip` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- Create syntax for TABLE 'projects'
 CREATE TABLE `projects` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `active` tinyint(4) unsigned NOT NULL DEFAULT '1',
+  `hateness` int(11) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `image` varchar(255) NOT NULL DEFAULT '',
   `url` varchar(255) NOT NULL DEFAULT '',
+  `slug` varchar(255) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- Create syntax for TABLE 'users'
 CREATE TABLE `users` (
