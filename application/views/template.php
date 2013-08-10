24de203825112a7923d50c4005f9105d91189412
Fabian Irsara <info@fabianirsara.com>
2013-08-10 14:00:15 +0200

    add analytics tracking code

--- application/views/template.php
+++ application/views/template.php
@@ -114,17 +114,17 @@
   <?php endif; ?>
 
 <?php if (Kohana::$environment === Kohana::PRODUCTION): ?>
-<?php /*
+
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-40733628-1', 'madebyfibb.com');
+ga('create', 'UA-40733628-3', 'rotzprojekt.com');
 ga('send', 'pageview');
 </script>
-*/ ?>
+
 <?php endif; ?>
   
 </body>
